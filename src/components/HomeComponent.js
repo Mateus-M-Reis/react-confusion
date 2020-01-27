4bbeedead0988955e225ffef37c1a1aaed1302b1
Mateus-M-Reis <mateusmreis92@gmail.com>
2020-01-27 02:14:01 -0300

    fetch post comments

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -39,8 +39,8 @@ function Home(props) {
 				</div>
 				<div className="col-12 col-md m-1">
                <RenderCard item={props.promotion} 
-                  isLoading={props.promoLoading} 
-                  errMess={props.promoErrMess} />
+                  isLoading={props.promosLoading} 
+                  errMess={props.promosErrMess} />
 				</div>
 				<div className="col-12 col-md m-1">
 					<RenderCard item={props.leader} />
